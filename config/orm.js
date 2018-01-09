feb0ae08c4c55f551dfbe70175bf2d95cdfa4f1a
Nicholas Vicchrilli <nvicchrilli@nvicchrilli-l.corp.instructure.com.corp.instructure.com>
2018-01-08 18:08:51 -0700

    first push

---
+++ config/orm.js
@@ -0,0 +1,4 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+//Need to create methods that will execute the necessary My SQL commands
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Tony Burger", false, "2017-12-06 05:25:00"),
+  ("Small Char", false, "2017-12-06 05:26:01"),
+  ("Hires Big H", false, "2017-12-06 05:27:02");
+  
