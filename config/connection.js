6df77ee01354cf49376a1f22677b2052d410dc5a
Nicholas Vicchrilli <nvicchrilli@nvicchrilli-l.corp.instructure.com.corp.instructure.com>
2018-03-11 15:39:59 -0600

    spelling fixes

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,14 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    user: "roo",
+    user: "root",
     password: "",
     database: "burgers_db"
   });
