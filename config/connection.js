487c1b7d10f0d3257961c1d157d6c36765547f7a
Nicholas Vicchrilli <nvicchrilli@nvicchrilli-l.corp.instructure.com.corp.instructure.com>
2018-03-11 14:42:50 -0600

    Final touches for Burger

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,6 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  port: 3030,
   host: "localhost",
   user: "root",
   password: "",
@@ -11,11 +10,8 @@ var connection = mysql.createConnection({
 
 // Make connection.
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+  if (err)throw err;
+  console.log("connected as id " + connection.threadid);
 });
 
 // Export connection for our ORM to use.
--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var handlebars = require('handlebars');
 
-var PORT = process.env.PORT || 3030;
 
 var app = express();
+var port = 3030;
+
+
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+app.use(methodOverride("_method"));
+app.engine('handlebars',exphbs({
+  defaultLayout: 'main'
+}));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/routes.js");
+app.use('/', routes);
 
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+app.listen(port, function() {
+  console.log("App now listening at localhost:" + port);
 });
